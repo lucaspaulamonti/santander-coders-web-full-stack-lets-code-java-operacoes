640ce9caef36debb381c860807f860815194c4a3
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-09-18 15:34:24 -0300

    Initial commit

---
+++ operacoes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//        Realize soma, subtração, multiplicação e divisão utilizando cast para os valores inteiros 3 e 2.
+        int a = 3;
+        int b = 2;
+        int soma = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        float div = (float) a / b;
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+    }
+}
